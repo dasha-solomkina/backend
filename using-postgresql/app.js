d3b6f457ec8a92b239cd3aca5c7a8c8755955d5b
darialiit <solomkina.daria@gmail.com>
2024-08-21 14:30:59 -0300

    Using PostgreSQL exerscise p.2

--- using-postgresql/app.js
+++ using-postgresql/app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const indexRouter = require('./routes/index')
+require('dotenv').config()
+console.log(process.env)
 
 app.use(express.urlencoded({ extended: true }))
 app.use('/', indexRouter)
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
   console.log('Server running on http://localhost:3000')
 })
